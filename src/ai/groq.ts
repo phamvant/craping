10442129d396c7991a4a1c4cf9eb923c075f9d65
phamvant <pham.t.286.01@gmail.com>
2024-07-02 16:27:21 +0900

    api loop

--- src/ai/groq.ts
+++ src/ai/groq.ts
@@ -1,4 +1,5 @@
 import Groq from "groq-sdk";
+import fsSync from "fs";
 import fs from "fs/promises";
 
 const groq = new Groq({
@@ -28,13 +29,60 @@ async function getGroqChatCompletion(prop) {
 }
 
 const main = async () => {
-  const content = (
-    await fs.readFile("./result/MBA/MBA面试/000001.html")
-  ).toString();
-  const ret = await getGroqChatCompletion(content);
+  let i = 0;
+  const file = fsSync.readFileSync("contents.json");
+  const posts = JSON.parse(file.toString())["posts"] as string[];
+  let totalTokenPerMinues = 0;
 
-  console.log(ret.choices[0]?.message?.content || "");
-  console.log(ret);
+  const getData = async () => {
+    console.log(posts[i]);
+    const content = (await fs.readFile(`./result/${posts[i]}`)).toString();
+
+    const ret = await getGroqChatCompletion(content);
+
+    fs.writeFile(
+      `./markdown/${posts[i].toString().split("/")[2].replace("html", "md")}`,
+      ret.choices[0]?.message?.content || "null"
+    );
+    totalTokenPerMinues += ret.usage.total_tokens;
+
+    if (i < posts.length) {
+      i++;
+      return true;
+    } else {
+      false;
+    }
+  };
+
+  let isPending = false;
+  let pending;
+
+  while (1) {
+    if (isPending) {
+      while (new Date().getMinutes() - pending === 0) {}
+      isPending = false;
+    }
+
+    if (!(await getData())) {
+      return;
+    }
+
+    if (14400 - totalTokenPerMinues < 7000) {
+      isPending = true;
+      pending = new Date().getMinutes();
+    }
+  }
 };
 
 main();
+// const main = async () => {
+//   const content = (
+//     await fs.readFile("./result/MBA/MBA面试/000001.html")
+//   ).toString();
+//   const ret = await getGroqChatCompletion(content);
+
+//   console.log(ret.choices[0]?.message?.content || "");
+//   console.log(ret);
+// };
+
+// main();
