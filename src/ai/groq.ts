82b27df75e8678f71c7a9baa71d6c61f2d5b0c46
phamvant <pham.t.286.01@gmail.com>
2024-07-17 06:24:28 +0700

    add lmstudio api

--- src/ai/groq.ts
+++ src/ai/groq.ts
@@ -1,50 +1,58 @@
-import Groq from "groq-sdk";
-import fsSync from "fs";
-import fs from "fs/promises";
+import { readFile, writeFile } from "fs/promises";
+// import OpenAI from "openai";
 
-const groq = new Groq({
-  apiKey: process.env.AI_APIKEY,
-});
+import { LMStudioClient } from "@lmstudio/sdk";
 
-export async function callAI(prop) {
-  const chatCompletion = await getGroqChatCompletion(prop);
-  return chatCompletion.choices[0]?.message?.content || "";
-}
+const client = new LMStudioClient();
+
+// const openai = new OpenAI({
+//   baseURL: "http://localhost:1234/v1",
+//   apiKey: "lm-studio", // This is the default and can be omitted
+// });
+
+// export async function callAI(prop) {
+//   const chatCompletion = await getGroqChatCompletion(prop);
+//   return chatCompletion.choices[0]?.message?.content || "";
+// }
 
 async function getGroqChatCompletion(prop) {
-  return groq.chat.completions.create({
-    messages: [
-      {
-        role: "system",
-        content:
-          "You're helpful AI, I sent your artical as html, you will write summary in markdown in English",
-      },
-      {
-        role: "user",
-        content: prop,
-      },
-    ],
-    model: "llama3-8b-8192",
+  const modelPath = "bartowski/Starling-LM-7B-beta-GGUF";
+  const llama3 = await client.llm.load(modelPath, {
+    config: { gpuOffload: "max" },
   });
+
+  const prediction = llama3.respond([{ role: "user", content: prop }]);
+
+  return prediction;
 }
 
 const main = async () => {
   let i = 0;
-  const file = fsSync.readFileSync("contents.json");
+  const file = await readFile("contents.json");
   const posts = JSON.parse(file.toString())["posts"] as string[];
   let totalTokenPerMinues = 0;
 
   const getData = async () => {
     console.log(posts[i]);
-    const content = (await fs.readFile(`./result/${posts[i]}`)).toString();
+    const content = (await readFile(`./result/${posts[i]}`)).toString();
 
-    const ret = await getGroqChatCompletion(content);
+    const ret = await getGroqChatCompletion(
+      content +
+        `
 
-    fs.writeFile(
-      `./markdown/${posts[i].toString().split("/")[2].replace("html", "md")}`,
-      ret.choices[0]?.message?.content || "null"
+
+
+      translate to English in markdown.'
+      `,
     );
-    totalTokenPerMinues += ret.usage.total_tokens;
+
+    // console.log(ret.content);
+
+    writeFile(
+      `./markdown/${posts[i].toString().split("/")[2].replace("html", "md")}`,
+      ret.content || "null",
+    );
+    // totalTokenPerMinues += ret.usage.total_tokens;
 
     if (i < posts.length) {
       i++;
@@ -65,11 +73,6 @@ const main = async () => {
 
     if (!(await getData())) {
       return;
-    }
-
-    if (14400 - totalTokenPerMinues < 7000) {
-      isPending = true;
-      pending = new Date().getMinutes();
     }
   }
 };
